b2a01d6a87e767ee0f94afa6025640057ca74c60
Bahriddin <bahridnurullav@gmail.com>
2025-05-11 21:43:32 +0500

    dedicated lines form

--- app/dedicated-lines/page.tsx
+++ app/dedicated-lines/page.tsx
@@ -113,7 +113,7 @@ const JonPositions: React.FC<JonPositionsProps> = ({
           //   pathname: "/dedicated-lines-form",
           //   query: {jobTitle: title, schedule: schedule},
           // }}
-          href={"/dedicated-form"}>
+          href={"dedicatedlines-form"}>
           <button className="h-[45px] w-full bg-[#036760] hover:bg-[#02544f] transition rounded-sm text-[18px] text-white font-bold mt-4">
             Apply Now
           </button>
