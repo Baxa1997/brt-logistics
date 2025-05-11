131ea68abcf4488672e99cf6a4fef2dea5df9042
Bahriddin <bahridnurullav@gmail.com>
2025-05-11 22:13:48 +0500

    form removed

--- app/dedicated-lines/page.tsx
+++ app/dedicated-lines/page.tsx
@@ -108,12 +108,7 @@ const JonPositions: React.FC<JonPositionsProps> = ({
           </h3>
           <p className="text-sm text-gray-500">Dedicated Line Position</p>
         </div>
-        <Link
-          // href={{
-          //   pathname: "/dedicated-lines-form",
-          //   query: {jobTitle: title, schedule: schedule},
-          // }}
-          href={"dedicatedlines-form"}>
+        <Link href={"dedicatedlines-form"}>
           <button className="h-[45px] w-full bg-[#036760] hover:bg-[#02544f] transition rounded-sm text-[18px] text-white font-bold mt-4">
             Apply Now
           </button>
